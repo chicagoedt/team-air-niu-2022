753b947e2f5552d751e66cda7a66e120953887e8
TeamAirPi <edt.teamair@gmail.com>
2022-10-08 14:35:45 -0500

    fix formatting

--- Codebase/production/armMotor.py
+++ Codebase/production/armMotor.py
@@ -1,18 +1,18 @@
- from time import sleep
+from time import sleep
 
 def armMotor(control):
 
-    control.setMotor(-1):
+    control.setMotor(-1)
     
     print("Plug in battery!")
     
     for i in range(10):
-        control.setRGB(0,0,255):
+        control.setRGB(0,0,255)
         print(8-i)
         sleep(0.5)
-        control.setRGB(0,0,0):
+        control.setRGB(0,0,0)
         sleep(0.5)
 
-    control.setMotor(-0.6):
+    control.setMotor(-0.6)
     sleep(1)
-    control.setMotor(-1):
+    control.setMotor(-1)

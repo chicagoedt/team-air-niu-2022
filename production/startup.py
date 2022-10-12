17ae331c5bce876d5a7c0f3fa20f15d6594073e2
Adam Beigel <31280510+abeige@users.noreply.github.com>
2022-10-11 19:50:23 -0500

    wait for button press to run sorter

--- production/startup.py
+++ production/startup.py
@@ -1,8 +1,14 @@
 # add to /etc/rc.local:
 # python3 /home/pi/github/team-air/production/startup.py
 
-from armMotor import armMotor
-from control import Control
+from armMotor import *
+from sorter import *
+from control import *
+from time import sleep
+
+def buttonPressed():
+    global buttonHasBeenPressed
+    buttonHasBeenPressed = True
 
 control = Control()
 
@@ -11,3 +17,18 @@ control.button.wait_for_press()
 control.setRGB(0, 0, 0)
 
 armMotor(control)
+
+control.button.when_pressed = buttonPressed
+
+while (True):
+    global buttonHasBeenPressed
+    
+    control.setRGB(0, 0, 255)
+    sleep(0.5)
+    control.setRGB(0, 0, 0)
+    sleep(0.5)
+
+    if buttonHasBeenPressed:
+        break
+
+runSorter(control)
